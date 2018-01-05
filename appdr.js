d17a66f5a546377e35189c3bc9c45bca382746a7
subo93 <nsubodhi@gmail.com>
2018-01-05 11:51:40 +0530

    directives with scopes

--- appdr.js
+++ appdr.js
@@ -1,39 +1,52 @@
 name="MyAppdr";
 requires=[];
-
 appdr=angular.module(name,requires);
-
-
 
 appdr.controller("dirCtrl",function($scope){
     console.log("This is controller dirCtrl");
     $scope.employee={
-      name:"subodhi",
+      name:"subo",
       job:"cat",
       followers:["chin","adyakshaka","aluu"]
     }
+
+    $scope.employee2={
+      name:"suvi",
+      job:"dog",
+      followers:["aa","bb","ccc"]
+    }
+
     console.log("parent ",$scope);
-    /*
-$scope.follow=function(name){
-    $scope.employee.followers.push(name);
-}
-*/
 });
 
 appdr.directive("employeeCard",function(){
+  //$scope.employee={};
+
 return{
   templateUrl:'employee_info.html',
   restrict:"AEC",
-//  replace:true,
+  //replace:true,
+  scope:{
+    employee:"="
+  },
   controller:function($scope){
     $scope.follow=function(name){
         $scope.employee.followers.push(name);
   }
-
+    $scope.title="myTitleqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq";
+/*
+    $scope.employee={
+      name:"subo",
+      job:"cat",
+      followers:["chin","adyakshaka","aluu"]
+    }
+    */
+console.log("child ",$scope);
 },
-scope:true
-
 
 }
-console.log("dir  ",$scope);
+
 });
+//By assigning the scope value to true, the child scope itself can have seperdate values
+//By adding employee:"=" parent scope variables can access by the directive
+//
